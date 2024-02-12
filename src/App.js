f41210b32863c8e8e31bef5a80517e2f30916eb6
PIC <phenomena.in.code@gmail.com>
2024-02-12 13:44:28 +0100

    update index files and app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-function App() {}
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "leaflet/dist/leaflet.css";
+import Map from "./map";
+import UserPanel from "./userPanel";
+
+function App() {
+  // can I not link to index.css in index.html to be able to remove this?
+  document.body.style.position = "relative";
+
+  return (
+    <div className="flex-container">
+      <UserPanel></UserPanel>
+      <Map></Map>
+    </div>
+  );
+}
 
 export default App;
