14dfde2f373e2dff10db7789115e0fac14413268
PIC <phenomena.in.code@gmail.com>
2024-03-08 15:06:00 +0100

    add more markers to map, some of which are dependent on media query

--- src/map.js
+++ src/map.js
@@ -3,21 +3,13 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import { weatherMarker0 } from "./weatherMarker";
-import { weatherMarker1 } from "./weatherMarker";
-import { weatherMarker2 } from "./weatherMarker";
-import { weatherMarker3 } from "./weatherMarker";
-import { weatherMarker4 } from "./weatherMarker";
-import { weatherMarker5 } from "./weatherMarker";
-import { weatherMarker6 } from "./weatherMarker";
-import { weatherMarker7 } from "./weatherMarker";
+import { getIcon } from "./weatherMarkers";
 
 let defaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
 });
 
-// L.Marker.prototype.options.icon = weatherMarker;
 L.Marker.prototype.options.icon = defaultIcon;
 
 export default function Map({
@@ -29,22 +21,72 @@ export default function Map({
   showAdditionalMarkers,
   latitudeDevianceAdditionalMarkers,
   longitudeDevianceAdditionalMarkers,
+  highlightedDate,
 }) {
+  let zoomLevel;
+  if (window.matchMedia("(max-width: 700px)").matches) {
+    zoomLevel = 9;
+    console.log("smartph");
+  } else {
+    zoomLevel = 10;
+    console.log("geen smartph");
+  }
+
   const positionMarker1 = [
     position[0] + latitudeDevianceAdditionalMarkers,
-    [position[1] + longitudeDevianceAdditionalMarkers],
+    position[1] + longitudeDevianceAdditionalMarkers,
   ];
   const positionMarker2 = [
     position[0] + latitudeDevianceAdditionalMarkers,
-    [position[1] - longitudeDevianceAdditionalMarkers],
+    position[1] - longitudeDevianceAdditionalMarkers,
   ];
   const positionMarker3 = [
     position[0] - latitudeDevianceAdditionalMarkers,
-    [position[1] + longitudeDevianceAdditionalMarkers],
+    position[1] + longitudeDevianceAdditionalMarkers,
   ];
   const positionMarker4 = [
     position[0] - latitudeDevianceAdditionalMarkers,
-    [position[1] - longitudeDevianceAdditionalMarkers],
+    position[1] - longitudeDevianceAdditionalMarkers,
+  ];
+
+  const positionMarker5 = [
+    position[0] + 2 * latitudeDevianceAdditionalMarkers,
+    position[1],
+  ];
+
+  const positionMarker6 = [
+    position[0],
+    position[1] + 2 * longitudeDevianceAdditionalMarkers,
+  ];
+
+  const positionMarker7 = [
+    position[0] - 2 * latitudeDevianceAdditionalMarkers,
+    position[1],
+  ];
+
+  const positionMarker8 = [
+    position[0],
+    [position[1] - 2 * longitudeDevianceAdditionalMarkers],
+  ];
+
+  const positionMarker9 = [
+    position[0] + latitudeDevianceAdditionalMarkers,
+    [position[1] + 3 * longitudeDevianceAdditionalMarkers],
+  ];
+
+  const positionMarker10 = [
+    position[0] - latitudeDevianceAdditionalMarkers,
+    [position[1] + 3 * longitudeDevianceAdditionalMarkers],
+  ];
+
+  const positionMarker11 = [
+    position[0] + latitudeDevianceAdditionalMarkers,
+    [position[1] - 3 * longitudeDevianceAdditionalMarkers],
+  ];
+
+  const positionMarker12 = [
+    position[0] - latitudeDevianceAdditionalMarkers,
+    [position[1] - 3 * longitudeDevianceAdditionalMarkers],
   ];
 
   function ViewComponent() {
@@ -52,63 +94,95 @@ export default function Map({
     map.setView(position);
   }
 
-  function getIcon(weathercode) {
-    switch (true) {
-      case [0].includes(weathercode):
-        return weatherMarker0;
-      case [2].includes(weathercode):
-        return weatherMarker1;
-      case [3].includes(weathercode):
-        return weatherMarker2;
-      case [45, 48].includes(weathercode):
-        return weatherMarker3;
-      case [51, 56, 61, 66, 80].includes(weathercode):
-        return weatherMarker4;
-      case [53, 55, 63, 65, 57, 67, 81, 82].includes(weathercode):
-        return weatherMarker5;
-      case [71, 73, 75, 77, 85, 86].includes(weathercode):
-        return weatherMarker6;
-      case [95, 96, 99].includes(weathercode):
-        return weatherMarker7;
-      default:
-        console.log("Weathercode not found");
-    }
-  }
-
   return (
-    <MapContainer center={position} zoom={9}>
+    <MapContainer center={position} zoom={zoomLevel}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Open Street Map</a> contributors'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <ViewComponent />
-      {/* <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
       {showAdditionalMarkers && (
         <>
           <Marker
-            icon={getIcon(weatherData.daily.weathercode[0])}
+            icon={getIcon(weatherData.daily.weathercode[highlightedDate])}
             position={position}
           ></Marker>
           <Marker
-            icon={getIcon(additionalData.additional1.daily.weathercode[0])}
+            icon={getIcon(
+              additionalData.additional1.daily.weathercode[highlightedDate]
+            )}
             position={positionMarker1}
           ></Marker>
           <Marker
-            icon={getIcon(additionalData.additional2.daily.weathercode[0])}
+            icon={getIcon(
+              additionalData.additional2.daily.weathercode[highlightedDate]
+            )}
             position={positionMarker2}
           ></Marker>
           <Marker
-            icon={getIcon(additionalData.additional3.daily.weathercode[0])}
+            icon={getIcon(
+              additionalData.additional3.daily.weathercode[highlightedDate]
+            )}
             position={positionMarker3}
           ></Marker>
           <Marker
-            icon={getIcon(additionalData.additional4.daily.weathercode[0])}
+            icon={getIcon(
+              additionalData.additional4.daily.weathercode[highlightedDate]
+            )}
             position={positionMarker4}
           ></Marker>
+          <Marker
+            icon={getIcon(
+              additionalData.additional5.daily.weathercode[highlightedDate]
+            )}
+            position={positionMarker5}
+          ></Marker>
+          <Marker
+            icon={getIcon(
+              additionalData.additional6.daily.weathercode[highlightedDate]
+            )}
+            position={positionMarker6}
+          ></Marker>
+          <Marker
+            icon={getIcon(
+              additionalData.additional7.daily.weathercode[highlightedDate]
+            )}
+            position={positionMarker7}
+          ></Marker>
+          <Marker
+            icon={getIcon(
+              additionalData.additional8.daily.weathercode[highlightedDate]
+            )}
+            position={positionMarker8}
+          ></Marker>
+          {zoomLevel === 10 && (
+            <>
+              <Marker
+                icon={getIcon(
+                  additionalData.additional9.daily.weathercode[highlightedDate]
+                )}
+                position={positionMarker9}
+              ></Marker>
+              <Marker
+                icon={getIcon(
+                  additionalData.additional10.daily.weathercode[highlightedDate]
+                )}
+                position={positionMarker10}
+              ></Marker>
+              <Marker
+                icon={getIcon(
+                  additionalData.additional11.daily.weathercode[highlightedDate]
+                )}
+                position={positionMarker11}
+              ></Marker>
+              <Marker
+                icon={getIcon(
+                  additionalData.additional12.daily.weathercode[highlightedDate]
+                )}
+                position={positionMarker12}
+              ></Marker>
+            </>
+          )}
         </>
       )}
     </MapContainer>
